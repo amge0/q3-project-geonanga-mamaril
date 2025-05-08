be9487f9a71b5f2c086db057f5a8af3bd51ed99e
altyaur <b2028amggeonanga@pshs.edu.ph>
2025-05-09 02:11:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Step 3: Require/Loads the express module
+// Requires/Loads the express module
 const express = require('express');
 // body-parser is used to read data payload from the http request body
 const bodyParser = require('body-parser'); 
@@ -7,7 +7,7 @@ const path = require('path');
 // include the defined package
 
 
-// Step 4: Creates our express server
+// Creates express server
 const app = express();
 
 //Serves static files inside the public folder
@@ -15,13 +15,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sets a basic route index.hbs when website initially starts and when home is clicked from the nav bar or whenever a process needs to go back to home 
+// Sets a basic route index.hbs when website initially starts and when home is clicked from the nav bar or whenever a process needs to go back to home 
 app.get('/', (req, res) => {
     res.render('index.hbs');
 })
 
 
-// Step 5: Start HTTP Server on a port number 3000
+// Start HTTP Server on a port number 3000
 // This will create a web service for your own project
 const port = 3000;
 app.listen(port, () => console.log(`App listening to port ${port}`));
