1ae007c0bfb7f53d17501b749dca7677ff2f25c9
altyaur <b2028amggeonanga@pshs.edu.ph>
2025-05-09 19:35:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,107 @@
-// Requires/Loads the express module
 const express = require('express');
-// body-parser is used to read data payload from the http request body
-const bodyParser = require('body-parser'); 
-//  path is used to set default directories for MVC and also for the static files
-const path = require('path'); 
-// include the defined package
+const path = require('path');
+const fs = require('fs'); 
+const app = express(); 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// Creates express server
-const app = express();
-
-//Serves static files inside the public folder
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 
-// Sets a basic route index.hbs when website initially starts and when home is clicked from the nav bar or whenever a process needs to go back to home 
+const usersFilePath = path.join(__dirname, 'data', 'users.json');
+if (!fs.existsSync(path.join(__dirname, 'data'))) {
+  console.log('Creating data directory...');
+  fs.mkdirSync(path.join(__dirname, 'data'));
+}
+if (!fs.existsSync(usersFilePath)) {
+  console.log('Creating users.json file...');
+  fs.writeFileSync(usersFilePath, '{}');
+}
+
 app.get('/', (req, res) => {
-    res.render('index.hbs');
+    try {
+    const users = JSON.parse(fs.readFileSync(usersFilePath));
+    let user = req.query;
+    // add the new name and his/her score
+    user[user.username] = {username : user.username, password: user.password};
+    
+    console.log('Saving updated user data...');
+    fs.writeFileSync(usersFilePath, JSON.stringify(leadboard, null, 2));
+    console.log('User data saved successfully');
+    
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error reading users file:', error);
+    res.status(500).send('Error loading user data');
+  }
+});
 })
 
+app.get('/edit', (req, res) => {
+  try {
+    const leadboard = JSON.parse(fs.readFileSync(usersFilePath));
+    let user = req.query;
+    // find the correct key to change
+    for (let key in user) {
+      leadboard[key].score = Number(user[key]);
+    }
+    
+    console.log('Saving updated user data...');
+    fs.writeFileSync(usersFilePath, JSON.stringify(leadboard, null, 2));
+    console.log('User data saved successfully');
+    
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error reading users file:', error);
+    res.status(500).send('Error loading user data');
+  }
+  
+});
+
+app.get('/edit', (req, res) => {
+  try {
+    const leadboard = JSON.parse(fs.readFileSync(usersFilePath));
+    let user = req.query;
+    // find the correct key to change
+    for (let key in user) {
+      leadboard[key].score = Number(user[key]);
+    }
+    
+    console.log('Saving updated user data...');
+    fs.writeFileSync(usersFilePath, JSON.stringify(leadboard, null, 2));
+    console.log('User data saved successfully');
+    
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error reading users file:', error);
+    res.status(500).send('Error loading user data');
+  }
+  
+});
+
+// Deleting an entry from the leaderboard
+app.get('/delete', (req, res) => {
+  try {
+    const leadboard = JSON.parse(fs.readFileSync(usersFilePath));
+    let user = req.query;
+    // find the correct key to change
+    for (let key in user) {
+      delete leadboard[key];
+    }
+    
+    console.log('Saving updated user data...');
+    fs.writeFileSync(usersFilePath, JSON.stringify(leadboard, null, 2));
+    console.log('User data saved successfully');
+    
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error reading users file:', error);
+    res.status(500).send('Error loading user data');
+  }
+  
+});
 
 // Start HTTP Server on a port number 3000
 // This will create a web service for your own project
